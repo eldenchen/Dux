9ad153246a9bfa8100d20741acfd94195a94aeab
Abhi Beckert <abhi@abhibeckert.com>
2013-04-26 07:52:03 +1000

    closes #97 fix CSS syntax highlighting when semicolon is missing

--- Dux/DuxCSSPropertyValueElement.m
+++ Dux/DuxCSSPropertyValueElement.m
@@ -27,7 +27,7 @@ static DuxCSSColorValueElement *colorValueElement;
 {
   [super initialize];
   
-  nextElementCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"/;#-0123456789"];
+  nextElementCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"/;#-0123456789}\n"];
   numericCharacterSet = [NSCharacterSet decimalDigitCharacterSet];
   
   commentElement = [DuxCSSCommentElement sharedInstance];
@@ -84,6 +84,8 @@ static DuxCSSColorValueElement *colorValueElement;
       *nextElement = commentElement;
       return foundCharacterSetRange.location - startingAt;
     case ';':
+    case '}':
+    case '\n':
       return foundCharacterSetRange.location - startingAt;
     case '#':
       *nextElement = colorValueElement;
